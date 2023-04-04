4cd7a69cdccef3236a800eb87bc70c20de419367
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-04-04 19:02:24 +0300

    create new block

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,21 @@
-function Blockchain(){
-    this.chain = [];
-    this.newTransaction = [];
+function Blockchain() {
+  this.chain = [];
+  this.newTransaction = [];
 }
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransaction,
+    nonce,
+    previousBlockHash: previousBlockHash,
+  };
+
+  (this.newTransaction = []), thi.chain.push(newBlock);
+  return newBlock;
+};
