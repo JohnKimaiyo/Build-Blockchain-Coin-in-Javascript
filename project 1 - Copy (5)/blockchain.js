45f05d33b715124b709aa875daad89162b9e948e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-04-05 18:03:03 +0300

    bloch

---
+++ project 1 - Copy (5)/blockchain.js
@@ -0,0 +1,20 @@
+function Blockchain() {
+  this.chain = [];
+  this.newTransaction = [];
+}
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransaction,
+    nonce,
+    prevousBlockHash: previousBlockHash,
+  };
+  (this.newTransaction = []), this.chain.push(newBlock);
+  return newBlock;
+};
